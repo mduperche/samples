06f3fbb64acc0051859077064f24cca261ca082d
Mathieu Duperche <mathieuduperche@gmail.com>
2020-04-22 07:28:30 -0400

    njkwqhil

---
+++ Sitecore/IntegrationTest/WebApplication.Tests/UnitTests/FakeDbTest.cs
@@ -0,0 +1,21 @@
+﻿using Sitecore.FakeDb;
+using Xunit;
+
+namespace WebApplication.Tests.UnitTests
+{ 
+    public class FakeDbTest
+    {
+        [Fact]
+        public void HowToCreateSimpleItem()
+        {
+            using (var db = new Db
+            {
+                new DbItem("Home") { { "Title", "Welcome!" } }
+            })
+            {
+                Sitecore.Data.Items.Item home = db.GetItem("/sitecore/content/home");
+                Xunit.Assert.Equal("Welcome!", home["Title"]);
+            }
+        }
+    }
+}
